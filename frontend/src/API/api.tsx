a25f4a7ad718fe4e898117f130786c862a1a31f6
EmilioCliff <emiliocliff@gmail.com>
2025-04-28 00:04:10 +0300

    feat: refactor API client configuration to enable headers for API key

--- frontend/src/API/api.tsx
+++ frontend/src/API/api.tsx
@@ -5,8 +5,9 @@ const api = generateClient({
 	endpoint: import.meta.env.VITE_AWS_GRAPHQL_ENDPOINT,
 	apiKey: import.meta.env.VITE_AWS_API_KEY,
 	// authToken: import.meta.env.VITE_AWS_AUTH_TOKEN,
-	// headers: {
-	// 'x-api-key': import.meta.env.VITE_AWS_API_KEY,
+	headers: {
+		'x-api-key': import.meta.env.VITE_AWS_API_KEY,
+	},
 });
 
 export default api;
