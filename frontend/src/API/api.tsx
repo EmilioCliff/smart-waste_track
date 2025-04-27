a3943abe203f6378b8d16a0c5b80d6d13e549a0e
EmilioCliff <emiliocliff@gmail.com>
2025-04-27 23:44:33 +0300

    feat: configure AWS API client with authentication mode

--- frontend/src/API/api.tsx
+++ frontend/src/API/api.tsx
@@ -1,5 +1,5 @@
 import { generateClient } from '@aws-amplify/api';
 
-const api = generateClient();
+const api = generateClient({ authMode: import.meta.env.VITE_AWS_AUTH_TYPE });
 
 export default api;
