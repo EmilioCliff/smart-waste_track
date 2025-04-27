eaa5caac1f11c7e903dbde7841469f9571d8bfa7
EmilioCliff <emiliocliff@gmail.com>
2025-04-27 23:58:19 +0300

    feat: enhance AWS API client configuration with endpoint and API key

--- frontend/src/API/api.tsx
+++ frontend/src/API/api.tsx
@@ -1,5 +1,12 @@
 import { generateClient } from '@aws-amplify/api';
 
-const api = generateClient({ authMode: import.meta.env.VITE_AWS_AUTH_TYPE });
+const api = generateClient({
+	authMode: import.meta.env.VITE_AWS_AUTH_TYPE,
+	endpoint: import.meta.env.VITE_AWS_GRAPHQL_ENDPOINT,
+	apiKey: import.meta.env.VITE_AWS_API_KEY,
+	// authToken: import.meta.env.VITE_AWS_AUTH_TOKEN,
+	// headers: {
+	// 'x-api-key': import.meta.env.VITE_AWS_API_KEY,
+});
 
 export default api;
